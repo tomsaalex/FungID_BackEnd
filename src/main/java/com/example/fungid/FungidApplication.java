9408ae622f401f555dfd85c2b2bdbb9f09d5d482
Tomșa Alexandru-Eduard <tomsaalex@gmail.com>
2024-07-15 16:20:48 +0300

    Tests partially done, but could be made more elgant. Also changed the ImageRepository a bit to allow for mocking the FileSystem.

--- src/main/java/com/example/fungid/FungidApplication.java
+++ src/main/java/com/example/fungid/FungidApplication.java
@@ -2,9 +2,6 @@ package com.example.fungid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
